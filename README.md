7cf8743c1c5deee734f32b39b5e7cb099891f517
kushal-kamra <kushalkamradev@gmail.com>
2023-06-02 08:44:59 +0530

    Solution

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # store-checkout-queue
 Store Checkout Queue in React
+
+Source - by Web Dev Cody - https://www.youtube.com/watch?v=B9fmr1TpKHE
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [itemsInPersonCart, setItemsInPersonCart] = useState(0);
+  const [lines, setLines] = useState([
+    [10, 5, 2],[1],[2],[3],[4]
+  ]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLines(prevLines => {
+        return prevLines.map(line => {
+          return [line[0] - 1, ...line.slice(1)].filter((value) => value > 0);
+        })
+      })
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, [])
+
+  function addPersonToLine(e) {
+    e.preventDefault();
+
+    let leastItemsAmount = 1e9;
+    let lineWithLeast;
+
+    for (let line of lines) {
+      const totalInLine = line.reduce((sum, value) => sum + value, 0);
+
+      if (totalInLine < leastItemsAmount) {
+        leastItemsAmount = totalInLine;
+        lineWithLeast = line;
+      }
+    }
+
+    if (!lineWithLeast) return;
+
+    setLines(prevLines => prevLines.map(line => {
+      if (line === lineWithLeast) {
+        return [...line, itemsInPersonCart];
+      } else {
+        return line;
+      }
+    }))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="App">
+        <form onSubmit={addPersonToLine}>
+          <input
+            required
+            type="number"
+            value={itemsInPersonCart}
+            onChange={(e) => setItemsInPersonCart(e.currentTarget.valueAsNumber || 0)}
+          ></input>
+          <button>Checkout</button>
+        </form>
+        <div className="lines">
+          {lines.map((line, idx) => (
+            <div key={idx} className="line">
+            {line.map((numberOfItems, index) => (
+              <div key={index}>{numberOfItems}</div>
+            ))}
+            </div>
+          ))}
+        </div>
+      </main>
     </>
   )
 }
 
-export default App
+export default App;
